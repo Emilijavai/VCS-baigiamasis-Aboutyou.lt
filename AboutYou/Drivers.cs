2013ab9820e4663b4931dd6d8debf76e4dc618dc
Emilijavai <64403098+Emilijavai@users.noreply.github.com>
2020-06-20 20:26:40 +0300

    Add files via upload

---
+++ AboutYou/Drivers.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutYou
+{
+    public class Drivers
+    {
+        public static IWebDriver GetChromeDriver()
+        {
+            return GetDriver(Browsers.Chrome);
+        }
+
+        public static IWebDriver GetFireFoxDriver()
+        {
+            return GetDriver(Browsers.Firefox);
+        }
+
+        public static IWebDriver GetChromeWithOptions()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            options.AddArgument("incognito");
+
+            return new ChromeDriver(options);
+        }
+
+        private static IWebDriver GetDriver(Browsers browserName)
+        {
+            IWebDriver webDriver = null;
+
+            switch (browserName)
+            {
+                case Browsers.Firefox:
+                    webDriver = new FirefoxDriver();
+                    break;
+                case Browsers.Chrome:
+                    webDriver = GetChromeWithOptions();
+                    break;
+
+            }
+
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            return webDriver;
+        }
+
+
+       
+    }
+}
---
+++ AboutYou/MyScreenshot.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutYou
+{
+    public class MyScreenshot
+    {
+        public static void MakePhoto(IWebDriver webdriver)
+        {
+            Screenshot myScreenshot = webdriver.TakeScreenshot();
+
+            string screenShotDirectory =
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)));
+
+            string sceenShotFolder = Path.Combine(screenShotDirectory, "screenshots");
+
+            Directory.CreateDirectory(sceenShotFolder);
+
+            string screenShotName = $"{TestContext.CurrentContext.Test.Name}_{DateTime.Now:HH-mm}.png";
+
+            string screenshotPath = Path.Combine(sceenShotFolder, screenShotName);
+
+            myScreenshot.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+
+            Console.WriteLine($"Isssaugojom {screenshotPath}");
+        }
+
+
+    }
+}
